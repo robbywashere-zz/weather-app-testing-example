035295c850c5d642650f599a414ca7192a5e35bf
Robert Polana <robbywashere@users.noreply.github.com>
2017-07-01 14:48:48 -0700

    Update get-weather-middleware.js

--- get-weather-middleware.js
+++ get-weather-middleware.js
@@ -10,20 +10,13 @@ router.get('/weather', async (req, res) => {
   let ip = reqIp.getClientIp(req);
 
   try {
-
     let { latitude, longitude } = (await request({ uri: `http://geoip.nekudo.com/api/${ip}`, json: true })).location;
-
-
     let { currently } = await request({ uri: `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${latitude},${longitude}`, json: true })
-
     res.send(currently)
-
   } catch (e) {
     console.error(e);
     res.send(500, e.message);
   }
-
-
 });
 
 module.exports = router;
